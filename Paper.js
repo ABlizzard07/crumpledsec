ec840ae7c95e5523525475c24fe123db05a1f614
UniversalOverlord123 <68557624+UniversalOverlord123@users.noreply.github.com>
2020-08-10 22:27:49 -0500

    Add files via upload

--- Paper.js
+++ Paper.js
@@ -12,7 +12,7 @@ class Paper{
         this.height = 60;
         this.diameter = diameter;
         
-        this.image = loadImage("paper.png");
+        this.image = loadImage("crumpledsec/blob/master/paper.png");
         World.add(world,this.body);
     }
     display(){
