e6ebb42b895f2774ccec1c4d6e9bea266641fe0a
UniversalOverlord123 <68557624+UniversalOverlord123@users.noreply.github.com>
2020-08-11 22:10:27 -0500

    Add files via upload

--- trash.js
+++ trash.js
@@ -8,6 +8,10 @@ class Trash{
         this.width = 150;
         this.height = 150;
         this.image = loadImage("./dustbingreen.png");
+        this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true}) 
+        this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true}) 
+        this.rightWallBody=Bodies.rectangle(x+width/2, y-height/2, wallThickness,height,options);
+
 
         World.add(world,this.body);
         World.add(world,this.image);
