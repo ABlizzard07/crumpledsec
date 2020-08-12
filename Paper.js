43524b407c575579b2d24a7e7cb05d2fe071dffa
UniversalOverlord123 <68557624+UniversalOverlord123@users.noreply.github.com>
2020-08-12 10:47:24 -0500

    Add files via upload

--- Paper.js
+++ Paper.js
@@ -7,11 +7,12 @@ class Paper{
             density: 2.4
         }
         
-        this.body = Bodies.circle(x,y,(this.diameter-20)/2,options);
+        this.body = Bodies.circle(x,y,diameter,options);
         this.width = 60;
         this.height = 60;
-        this.diameter = diameter;
+        this.diameter = 150;
         
+     //   this.image = loadImage("sprites/paper.png");
         this.image = loadImage("./paper.png");
         World.add(world,this.body);
     }
@@ -20,7 +21,7 @@ class Paper{
        push();
        translate(this.body.position.x, this.body.position.y);
        imageMode(CENTER);
-       image(this.image, 0, 0, this.width, this.height);
+       image(this.image, 0, 0, (this.diameter -20)/2, this.height);
        pop();
        
     }
